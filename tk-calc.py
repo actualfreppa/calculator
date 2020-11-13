b263aa7dd8263886276498c6a365b543eaadb872
Fredrik Svanström <fredrik@cfc.se>
2020-11-13 11:03:50 +0100

    Added to Base

--- tk-calc.py
+++ tk-calc.py
@@ -5,10 +5,13 @@ from tkinter import ttk
 
 
 class Calculator:
-    pass
+    def __init__(self, master):
+        self.master = master
 
 def main():
-    pass
+    root = Tk()
+    app = Calculator(root)
+    root.mainloop()
 
 if __name__ == __"main":
     main()
